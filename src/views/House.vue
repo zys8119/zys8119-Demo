6201970616712d36a15e96163c01f6da7f7ab5c9
zys8119 <770959294@qq.com>
2023-03-29 15:47:54 +0800

    [add] 房子3d效果

--- src/views/House.vue
+++ src/views/House.vue
@@ -16,6 +16,7 @@
 <script setup lang="ts" title="房子" content="房子3d效果">
 import BaseThree, {BaseThreeClass, InitializationData} from "../components/BaseThree"
 import bj from "@/src/assets/house/bj2.png"
+import font from "@/src/assets/miaozidongmanti-regular.ttf?url"
 import {BoxGeometry, Mesh, Vector2, Raycaster} from "three"
 import {merge} from "lodash"
 import {TransformControls} from "three/addons/controls/TransformControls.js";
@@ -53,14 +54,14 @@ const addBoxConfig = {
     rotation:[0, 0, 0],
 }
 const changes = []
-const addBox = (three:BaseThreeClass, tr:TransformControls, data?:Partial<typeof addBoxConfig>)=>{
+const addBox = async (three:BaseThreeClass, tr:TransformControls, data?:Partial<typeof addBoxConfig>, text?:string)=>{
     const {scene, THREE} = three
-    const height = 80
+    const height = 70
     const y = height/2
     const {
         width,
         depth,
-        position:[x, z],
+        position:[x, z, py],
         scale:[sx, sy, sz],
         rotation:[rx, ry, rz],
     } = merge(addBoxConfig, data)
@@ -72,7 +73,8 @@ const addBox = (three:BaseThreeClass, tr:TransformControls, data?:Partial<typeof
             map
         })
     )
-    m.position.set(x,y ,z)
+    const positionY = py || y
+    m.position.set(x, positionY ,z)
     m.scale.set(sx, sy, sz)
     m.rotation.set(rx, ry, rz)
     m.castShadow = true
@@ -92,105 +94,226 @@ const addBox = (three:BaseThreeClass, tr:TransformControls, data?:Partial<typeof
     tr.addEventListener('change', change)
     changes.push(change)
     scene.add(m);
+    if(text){
+        const {mesh} = await three.addText(text, 'font')
+        console.clear()
+        console.log(Math.abs((height*sy+2)/2+positionY), x, positionY ,z, sx, sy, sz)
+        mesh.position.set(x,Math.abs((height*sy+2)/2+positionY) ,z)
+        mesh.scale.set(0.01594, 0.01594, 0.01594)
+        mesh.rotation.set(-1.5, 0, 0)
+        scene.add(mesh)
+    }
 }
+const modes = ref(['translate', 'scale', 'rotate'])
+const {a,s,d } = useMagicKeys()
 const load = async (three:BaseThreeClass)=>{
+    const ground = three.downloadImagesTexture("https://img1.baidu.com/it/u=4165515568,3899639356&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500", 'addBoxTexture')
+    await three.downloadFonts(font, 'font')
     const {THREE, planeGeometryMesh,scene, camera, light} = three
     light.position.set(0,500,200)
-    const map = three.downloadImagesTexture(bj);
-    const sprite = new THREE.Mesh(new THREE.BoxGeometry(300, 0, 300), new THREE.MeshLambertMaterial({
-        map:map,
-        emissive:"#f00",
-        transparent:true,
+    // 户型布局底图
+    // const map = three.downloadImagesTexture(bj);
+    // const sprite = new THREE.Mesh(new THREE.BoxGeometry(300, 1, 300), new THREE.MeshLambertMaterial({
+    //     map,
+    //     emissive:"#f00",
+    //     transparent:true,
+    // }));
+    // scene.add( sprite );
+    const sprite = new THREE.Mesh(new THREE.BoxGeometry(300, 1, 300), new THREE.MeshLambertMaterial({
+        map:ground,
     }));
     scene.add( sprite );
     const tr = three.transformControls()
     watchEffect(()=>{
-        tr.setMode(data.value.mode as any)
+        if(a.value){
+            tr.setMode(modes.value[0] as any)
+        }else if(s.value){
+            tr.setMode(modes.value[1] as any)
+        }else if(d.value){
+            tr.setMode(modes.value[2] as any)
+        }else {
+            tr.setMode(data.value.mode as any)
+        }
     })
     addBox(three, tr, {
         position:[44.39, -147],
         scale: [0.94],
-    })
+    }, '1')
     addBox(three, tr, {
         position:[98.03, -129.23],
         scale: [0.32],
         rotation: [0, 1.56],
-    })
+    }, '2')
     addBox(three, tr, {
         position:[-9.112, -76.06],
         scale: [0.262, 1, 1.491],
         rotation: [0, 1.56],
-    })
+    }, '3')
     addBox(three, tr, {
         position:[-6.75, -63.55],
         scale: [0.2768, 1, 1.491],
         rotation: [0, 0],
-    })
+    }, '4')
     addBox(three, tr, {
         position:[90.56, -63.55],
         scale: [0.991, 1, 1.491],
         rotation: [0, 0],
-    })
+    }, '5')
     addBox(three, tr, {
         position:[48.26, -74.505],
         scale: [0.1689, 1, 1.491],
         rotation: [0, 1.56],
-    })
+    }, '6')
     addBox(three, tr, {
         position:[59.56, -52.07],
         scale: [0.1689, 1, 1.491],
         rotation: [0, 1.56],
-    })
+    }, '7')
     addBox(three, tr, {
         position:[145.86, -14.966],
         scale: [0.8604, 1, 1.491],
         rotation: [0, 1.56],
-    })
+    }, '8')
     addBox(three, tr, {
         position:[139.567, -14.966],
         scale: [0.1279, 1, 1.491],
         rotation: [0, 0],
-    })
+    }, '9')
     addBox(three, tr, {
         position:[73.202, -14.966],
         scale: [0.6837, 1, 1.491],
         rotation: [0, 0],
-    })
+    }, '10')
     addBox(three, tr, {
         position:[110.912, -12.695],
         scale: [0.098099, 1, 1.491],
         rotation: [0, 1.56],
-    })
+    }, '11')
     addBox(three, tr, {
         position:[59.151, -4.766],
         scale: [0.223091, 1, 1.491],
         rotation: [0, 1.56],
-    })
+    }, '12')
     addBox(three, tr, {
         position:[35.846, -4.766],
         scale: [0.1566, 1, 1.491],
         rotation: [0, 1.56],
-    })
+    }, '13')
     addBox(three, tr, {
         position:[35.846, 89.17919],
         scale: [1.0263, 1, 1.491],
         rotation: [0, 1.56],
-    })
+    }, '14')
     addBox(three, tr, {
         position:[111.0753, 73.2094],
         scale: [1.0263, 1, 1.491],
         rotation: [0, 1.56],
-    })
+    }, '15')
     addBox(three, tr, {
         position:[87.0712, 33.708],
         scale: [0.467, 1, 1.491],
         rotation: [0, 0],
-    })
+    }, '16')
     addBox(three, tr, {
         position:[59.151, 33.09617],
         scale: [0.07228, 1, 1.491],
         rotation: [0, 1.56],
-    })
+    }, '17')
+    addBox(three, tr, {
+        position:[53.663, 140.3025],
+        scale: [0.20416, 1, 1.491],
+        rotation: [0, 1.56],
+    }, '18')
+    addBox(three, tr, {
+        position:[93.745, 140.3025],
+        scale: [0.20416, 1, 1.491],
+        rotation: [0, 1.56],
+    }, '19')
+    addBox(three, tr, {
+        position:[102.15, 131.4817],
+        scale: [0.2016, 1, 1.491],
+        rotation: [0, 0],
+    }, '20')
+    addBox(three, tr, {
+        position:[40.621, 131.4817],
+        scale: [0.2794, 1, 1.491],
+        rotation: [0, 0],
+    }, '21')
+    addBox(three, tr, {
+        position:[-47, 131.4817],
+        scale: [0.2164, 1, 1.491],
+        rotation: [0, 0],
+    }, '22')
+    addBox(three, tr, {
+        position:[-107.44, 131.4817],
+        scale: [0.1973, 1, 1.491],
+        rotation: [0, 0],
+    }, '23')
+    addBox(three, tr, {
+        position:[-46.8022, 89.17919],
+        scale: [1.0263, 1, 1.491],
+        rotation: [0, 1.56],
+    }, '24')
+    addBox(three, tr, {
+        position:[-115.813, 89.17919],
+        scale: [1.0263, 1, 1.491],
+        rotation: [0, 1.56],
+    }, '25')
+    addBox(three, tr, {
+        position:[-91.9445, 31.39],
+        scale: [0.3718, 1, 1.491],
+        rotation: [0, 0],
+    }, '26')
+    addBox(three, tr, {
+        position:[-118.8577, 51.203],
+        scale: [0.0756, 1, 1.491],
+        rotation: [0, 0],
+    }, '27')
+    addBox(three, tr, {
+        position:[-146.7509, 8.384],
+        scale: [1.2771, 1, 1.491],
+        rotation: [0, 1.56],
+    }, '28')
+    addBox(three, tr, {
+        position:[-83.8454, -31.301],
+        scale: [0.6195, 1, 1.491],
+        rotation: [0, 1.56],
+    }, '29')
+    addBox(three, tr, {
+        position:[-45.548, -31.301],
+        scale: [0.6195, 1, 1.491],
+        rotation: [0, 1.56],
+    }, '30')
+    addBox(three, tr, {
+        position:[-110.738, -64.833],
+        scale: [0.64934, 1, 1.491],
+        rotation: [0, 0],
+    }, '31')
+    addBox(three, tr, {
+        position:[-49.315, -64.833],
+        scale: [0.12904, 1, 1.491],
+        rotation: [0, 0],
+    }, '32')
+    addBox(three, tr, {
+        position:[-75.8735, 2.14],
+        scale: [0.1619, 1, 1.491],
+        rotation: [0, 0],
+    }, '33')
+    addBox(three, tr, {
+        position:[-74.1106, 5.48114],
+        scale: [0.04225, 1, 1.491],
+        rotation: [0, 1.56],
+    }, '34')
+    addBox(three, tr, {
+        position:[-32.7006, -63.7064, 15.9614],
+        scale: [0.0635, 0.4445, 3.8021],
+        rotation: [0, 1.56],
+    }, '35')
+    addBox(three, tr, {
+        position:[-32.7006, -63.7064 , 66.0017],
+        scale: [0.0635, 0.1139, 3.8021],
+        rotation: [0, 1.56],
+    }, '36')
     three.controls.addEventListener('change', ()=>{
         console.log("camera", {
             position:camera.position,
@@ -205,7 +328,7 @@ const load = async (three:BaseThreeClass)=>{
 const gui = (d: typeof data.value, three: BaseThreeClass) => {
     const {THREE} = three
     const cameraFolder = three.gui.addFolder('配置参数')
-    cameraFolder.add(d, 'mode', ['translate', 'rotate', 'scale'])
+    cameraFolder.add(d, 'mode', modes.value)
     return () => {
     }
 }
