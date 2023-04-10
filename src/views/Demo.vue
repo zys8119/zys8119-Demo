6f3f6645a6f86661640b3690ee9d3d823082c029
zys8119 <770959294@qq.com>
2023-04-10 13:47:18 +0800

    [add] 博客

---
+++ src/views/Demo.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="Demo">
+        Demo
+    </div>
+</template>
+
+<script setup lang="ts">
+import animation from "winframe"
+</script>
+
+<style scoped lang="less">
+.Demo {
+}
+</style>
--- src/views/threeMap.vue
+++ src/views/threeMap.vue
@@ -18,6 +18,7 @@ import font from "@/src/assets/miaozidongmanti-regular.ttf?url"
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
 import { Object3D, BoxGeometry, Mesh, MeshLambertMaterial} from 'three'
 import onEvent from "three-onevent-esm"
+import animationFun from "winframe"
 const data = ref({
     box: {
         position: {
@@ -214,56 +215,6 @@ const gui = (d: typeof data.value, three: BaseThreeClass) => {
     const cameraFolder = three.gui.addFolder('配置参数')
     cameraFolder.add(d, 'mode', modes.value)
     return () => {
-    }
-}
-const animationFn = (callback:(progress:number)=>boolean | void, timeout:number, sync?:boolean, resolve?:any, reject?:any) => {
-    try {
-        let isReturn = false
-        let ra:any = null
-        let startTime = Date.now()
-        const endTime = startTime + (timeout === 0 ? Infinity : timeout)
-        const fn = () => {
-            let progress = Number(((timeout - (endTime - startTime)) / timeout))
-            if (progress < 0) {progress = 0}
-            if (progress > 1) {progress = 1}
-            isReturn = callback(progress) as boolean
-            if (progress >= 1) {
-                if (!sync) {
-                    cancelAnimationFrame(ra)
-                }
-                resolve(progress)
-                return
-            }
-            startTime = Date.now()
-            if (sync) {
-                if (!isReturn) {
-                    ra = fn()
-                }
-            } else {
-                ra = requestAnimationFrame(fn)
-            }
-        }
-        isReturn = callback(0) as boolean
-        if (sync) {
-            if (!isReturn) {
-                ra = fn()
-            }
-        } else {
-            ra = requestAnimationFrame(fn)
-        }
-    } catch (e) {
-        reject(e)
-    }
-}
-const animationFun = (callback:(progress:number)=>boolean | void, timeout:number, sync?:boolean) => {
-    if (sync) {
-        animationFn(callback, timeout, sync, () => {}, (e:Error) => {
-            throw e
-        })
-    } else {
-        return new Promise((resolve, reject) => {
-            animationFn(callback, timeout, sync, resolve, reject)
-        })
     }
 }
 // 柱子动画
