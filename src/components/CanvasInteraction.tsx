32658dde9c8dbbc5f686b19d1529c5650e715d48
zys8119 <770959294@qq.com>
2023-04-20 16:52:55 +0800

    [add] canvas 交互

--- src/components/CanvasInteraction.tsx
+++ src/components/CanvasInteraction.tsx
@@ -1,19 +1,18 @@
 import Hammerjs from "hammerjs"
 import cssRender from 'css-render'
 import bem from '@css-render/plugin-bem'
-import { useSsrAdapter } from '@css-render/vue3-ssr'
-import { createCNode, CSelector } from 'css-render/lib/types'
+import {useSsrAdapter} from '@css-render/vue3-ssr'
+import {createCNode, CSelector} from 'css-render/lib/types'
+
 const cssr = cssRender()
 const plugin = bem({})
 cssr.use(plugin) // bind the plugin with the cssr instance
-const { cB, cE } = plugin
-const { c }: { c: createCNode<CSelector> } = cssr
-export const style = cB("CanvasInteraction",[
-    c('canvas',{
-        position:'absolute',
-        left:0,
-        top:0,
-        cursor:'default'
+const {cB, cE} = plugin
+const {c}: { c: createCNode<CSelector> } = cssr
+export const style = cB("CanvasInteraction", [
+    c('canvas', {
+        cursor: 'default',
+        border:'1px '
     })
 ])
 export const ssr: any = useSsrAdapter()
@@ -21,6 +20,7 @@ style.mount({
     id: 'CanvasInteraction',
     ssr
 })
+
 export interface ObjectBaseType {
     isInside?(): boolean
 
@@ -30,26 +30,26 @@ export interface ObjectBaseType {
 
     panmove?(event: any): [xName: string, yName: string]
 
-    readonly width?:number
-    readonly height?:number
-    readonly left?:number
-    readonly top?:number
-    readonly gapSize?:number
-    readonly gapSizeBlank?:number
-    readonly position?:string | void
-    readonly centerX?:number
-    readonly centerY?:number
-    x?:number
-    y?:number
-    w?:number
-    h?:number
+    readonly width?: number
+    readonly height?: number
+    readonly left?: number
+    readonly top?: number
+    readonly gapSize?: number
+    readonly gapSizeBlank?: number
+    readonly position?: string | void
+    readonly centerX?: number
+    readonly centerY?: number
+    x?: number
+    y?: number
+    w?: number
+    h?: number
 }
 
 const CanvasInteraction = defineComponent({
-    emits:{
-        load:()=> void 0
+    emits: {
+        load: () => void 0
     },
-    props:{
+    props: {
         gap: {
             type: Number,
             default: 10
@@ -62,8 +62,16 @@ const CanvasInteraction = defineComponent({
             type: Boolean,
             default: true
         },
+        width: {
+            type: Number,
+            default: null
+        },
+        height: {
+            type: Number,
+            default: null
+        },
     },
-    setup(props,{emit}) {
+    setup(props, {emit}) {
         const vm = getCurrentInstance()
         const canvasRef = ref()
         const canvas = $computed(() => canvasRef.value) as HTMLCanvasElement
@@ -72,46 +80,47 @@ const CanvasInteraction = defineComponent({
         const objectCache = ref([])
         const {x, y} = useMouseInElement(canvas)
         const {Shift} = useMagicKeys({
-            onEventFired(e){
+            onEventFired(e) {
                 console.log(e.key)
             }
         })
+        const isPan = ref(false)
         watchEffect(() => {
             if (canvas) {
-                canvas.width = width.value
-                canvas.height = height.value
+                canvas.width = props.width || width.value
+                canvas.height = props.height || height.value
             }
         })
         const positionMap = ref({
-            top_left:{
-                cursor:'nw-resize'
+            top_left: {
+                cursor: 'nw-resize'
             },
-            top_center:{
-                cursor:'n-resize'
+            top_center: {
+                cursor: 'n-resize'
             },
-            top_right:{
-                cursor:'ne-resize'
+            top_right: {
+                cursor: 'ne-resize'
             },
-            bottom_left:{
-                cursor:'sw-resize'
+            bottom_left: {
+                cursor: 'sw-resize'
             },
-            bottom_center:{
-                cursor:'s-resize'
+            bottom_center: {
+                cursor: 's-resize'
             },
-            bottom_right:{
-                cursor:'se-resize'
+            bottom_right: {
+                cursor: 'se-resize'
             },
-            left_center:{
-                cursor:'w-resize'
+            left_center: {
+                cursor: 'w-resize'
             },
-            right_center:{
-                cursor:'e-resize'
+            right_center: {
+                cursor: 'e-resize'
             },
-            content:{
-                cursor:'move'
+            content: {
+                cursor: 'move'
             },
-            blank:{
-                cursor:'pointer'
+            blank: {
+                cursor: 'pointer'
             },
         })
 
@@ -128,11 +137,12 @@ const CanvasInteraction = defineComponent({
                 return false
             }
 
-            get gapSize(){
-                return props.gap+props.gapLineWidth*2
+            get gapSize() {
+                return props.gap + props.gapLineWidth * 2
             }
-            get gapSizeBlank(){
-                return this.gapSize*1.5
+
+            get gapSizeBlank() {
+                return this.gapSize * 1.5
             }
 
             get width() {
@@ -159,37 +169,37 @@ const CanvasInteraction = defineComponent({
                 return this.y + this.h / 2
             }
 
-            get position(){
+            get position() {
                 const sx = x.value - this.x
                 const sy = y.value - this.y
-                const isTop = sy>= -this.gapSizeBlank && sy <= this.gapSize - this.gapSizeBlank
-                const isBottom = sy>= this.height+this.gapSize*0.5 && sy <= this.height  + this.gapSizeBlank
+                const isTop = sy >= -this.gapSizeBlank && sy <= this.gapSize - this.gapSizeBlank
+                const isBottom = sy >= this.height + this.gapSize * 0.5 && sy <= this.height + this.gapSizeBlank
                 const isLeft = sx >= -this.gapSizeBlank && sx <= this.gapSize - this.gapSizeBlank
-                const isRight = sx >= this.width + this.gapSize*0.5 && sx <= this.width + this.gapSize*1.5
-                const isWidthCenter = sx >= this.width / 2 - this.gapSize*0.5 && sx <= this.width / 2 + this.gapSize*0.5
-                const isHeightCenter = sy >= this.height / 2 - this.gapSize*0.5 && sy <= this.height / 2 + this.gapSize*0.5
-                if(isLeft && isTop){
+                const isRight = sx >= this.width + this.gapSize * 0.5 && sx <= this.width + this.gapSize * 1.5
+                const isWidthCenter = sx >= this.width / 2 - this.gapSize * 0.5 && sx <= this.width / 2 + this.gapSize * 0.5
+                const isHeightCenter = sy >= this.height / 2 - this.gapSize * 0.5 && sy <= this.height / 2 + this.gapSize * 0.5
+                if (isLeft && isTop) {
                     return 'top_left'
                 }
-                if(isWidthCenter && isTop){
+                if (isWidthCenter && isTop) {
                     return 'top_center'
                 }
-                if(isRight && isTop){
+                if (isRight && isTop) {
                     return 'top_right'
                 }
-                if(isLeft &&  isBottom){
+                if (isLeft && isBottom) {
                     return 'bottom_left'
                 }
-                if(isWidthCenter && isBottom){
+                if (isWidthCenter && isBottom) {
                     return 'bottom_center'
                 }
-                if(isRight && isBottom){
+                if (isRight && isBottom) {
                     return 'bottom_right'
                 }
-                if(isLeft && isHeightCenter){
+                if (isLeft && isHeightCenter) {
                     return 'left_center'
                 }
-                if(isRight && isHeightCenter){
+                if (isRight && isHeightCenter) {
                     return 'right_center'
                 }
                 return sx > 0 && sx < this.w && sy > 0 && sy < this.h ? 'content' : 'blank'
@@ -261,12 +271,12 @@ const CanvasInteraction = defineComponent({
 
         const init = async () => {
             await vm.vnode.props.onLoad?.({
-                ObjectsClass:{
-                    Rect:createRect,
-                    Image:createImage,
+                ObjectsClass: {
+                    Rect: createRect,
+                    Image: createImage,
                 },
                 ObjectBase,
-                scene:objectCache.value,
+                scene: objectCache.value,
                 x,
                 y,
                 width,
@@ -278,14 +288,18 @@ const CanvasInteraction = defineComponent({
             await hammerInit()
         }
         const currObject = computed(() => {
-            let object = null
-            for (let lng = objectCache.value.length - 1, k = lng; k >= 0; k--) {
-                if (objectCache.value[k].isInside()) {
-                    object = objectCache.value[k]
-                    break
+            let object =  null
+            if(isPan.value){
+                return  currObject.value
+            }else {
+                for (let lng = objectCache.value.length - 1, k = lng; k >= 0; k--) {
+                    if (objectCache.value[k].isInside()) {
+                        object = objectCache.value[k]
+                        break
+                    }
                 }
+                return object
             }
-            return object
         })
 
         const canvasStyle = computed(() => {
@@ -293,6 +307,20 @@ const CanvasInteraction = defineComponent({
                 cursor: props.showHelp && currObject.value ? positionMap.value[currObject.value.position]?.cursor : null
             }
         })
+
+        const getScaleRatio = (w: number, h: number, mw: number, mh: number) => {
+            let ratio = 1
+            if (w > mw) {
+                ratio = mw / w
+            } else if (h > mh) {
+                ratio = mh / h
+            } else if (mw > w) {
+                ratio = mw / w
+            } else {
+                ratio = mh / h
+            }
+            return ratio
+        }
 
 
         const hammerInit = async () => {
@@ -304,9 +332,10 @@ const CanvasInteraction = defineComponent({
             let object = null
             let position = null
             hammer.get('pan').set({
-                enable(a, event){
-                    if(event){
-                        if(event.isFirst){
+                enable(a, event) {
+                    if (event) {
+                        if (event.isFirst) {
+                            isPan.value = true
                             object = currObject.value
                             if (object) {
                                 if (object?.panstart) {
@@ -321,43 +350,92 @@ const CanvasInteraction = defineComponent({
                                 h = object.h
                                 position = object.position
                             }
-                        } else if(event.isFinal){
+                        } else if (event.isFinal) {
                             object = null
                             position = null
+                            isPan.value = false
                         } else {
                             if (object) {
                                 switch (position) {
                                     case 'top_left':
+                                        if (Shift.value) {
+                                            const ratio = getScaleRatio(w, h, w - event.deltaX, h - event.deltaY)
+                                            object.w = w * ratio;
+                                            object.h = h * ratio;
+                                            object.x = x + w * (1-ratio)
+                                            object.y = y + h * (1-ratio)
+                                            return
+                                        }
                                         object.x = x + event.deltaX
                                         object.y = y + event.deltaY
                                         object.w = w - event.deltaX
                                         object.h = h - event.deltaY
                                         break
                                     case 'top_center':
+                                        if (Shift.value) {
+                                            const ratio = getScaleRatio(w, h, w, h - event.deltaY)
+                                            object.x = x + x * (1 - ratio) / 2
+                                            object.w = w * ratio;
+                                        }
                                         object.y = y + event.deltaY
                                         object.h = h - event.deltaY
                                         break
                                     case 'top_right':
+                                        if (Shift.value) {
+                                            const ratio = getScaleRatio(w, h, w + event.deltaX, h - event.deltaY)
+                                            object.w = w * ratio;
+                                            object.h = h * ratio;
+                                            object.y = y + h * (1-ratio)
+                                            return
+                                        }
                                         object.y = y + event.deltaY
                                         object.w = w + event.deltaX
                                         object.h = h - event.deltaY
                                         break
                                     case 'right_center':
+                                        if (Shift.value) {
+                                            const ratio = getScaleRatio(w, h, w + event.deltaX, h)
+                                            object.y = y + y * (1 - ratio) / 2
+                                            object.h = h * ratio;
+                                        }
                                         object.w = w + event.deltaX
                                         break
                                     case 'bottom_right':
+                                        if (Shift.value) {
+                                            const ratio = getScaleRatio(w, h, w + event.deltaX, h + event.deltaY)
+                                            object.w = w * ratio;
+                                            object.h = h * ratio;
+                                            return
+                                        }
                                         object.w = w + event.deltaX
                                         object.h = h + event.deltaY
                                         break
                                     case 'bottom_center':
+                                        if (Shift.value) {
+                                            const ratio = getScaleRatio(w, h, w, h + event.deltaY)
+                                            object.x = x + x * (1 - ratio) / 2
+                                            object.w = w * ratio;
+                                        }
                                         object.h = h + event.deltaY
                                         break
                                     case 'bottom_left':
+                                        if (Shift.value) {
+                                            const ratio = getScaleRatio(w, h, w - event.deltaX, h + event.deltaY)
+                                            object.w = w * ratio;
+                                            object.h = h * ratio;
+                                            object.x = x + x * (1 - ratio)
+                                            return
+                                        }
                                         object.x = x + event.deltaX
                                         object.w = w - event.deltaX
                                         object.h = h + event.deltaY
                                         break
                                     case 'left_center':
+                                        if (Shift.value) {
+                                            const ratio = getScaleRatio(w, h, w - event.deltaX, h)
+                                            object.y = y + y * (1 - ratio) / 2
+                                            object.h = h * ratio;
+                                        }
                                         object.x = x + event.deltaX
                                         object.w = w - event.deltaX
                                         break
