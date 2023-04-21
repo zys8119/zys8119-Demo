a96382f7769bb816f008bfd48d1a466d60e8bc89
zys8119 <770959294@qq.com>
2023-04-21 10:45:15 +0800

    [add] canvas 交互

--- src/components/CanvasInteraction.tsx
+++ src/components/CanvasInteraction.tsx
@@ -21,10 +21,59 @@ style.mount({
     ssr
 })
 
+// 代码
+class svgToBezierCurve {
+    dArr = [];
+    config:any = {
+        fill:true,
+        offsetX:0,
+        offsetY:0,
+    };
+    /**
+     * svg paths 转 canvas 贝塞曲线
+     * @param d
+     * @param w
+     * @param h
+     * @returns {*}
+     */
+    constructor(d, config) {
+        this.config = Object.assign(this.config, config)
+        this.dArr = d.match(/[a-z][^a-z]+/img);
+        this.dArr = this.dArr.map(e=>e.match(/(\w|\.|-)*/img).filter(e=>e))
+    }
+
+    /**
+     * 获取 贝塞曲线
+     */
+    getBezierCurve(){
+        let result = this.dArr.map(e=>{
+            const n = e.map(e=>Number(e.replace(/[^0-9.-]/img,""))).map((e,k)=>{
+                const xs:any = Object.prototype.toString.call(this.config.offsetX === "[object String]") ? "+":0;
+                const ys:any = Object.prototype.toString.call(this.config.offsetY === "[object String]") ? "+":0;
+                return e+(k % 2 ? ys + this.config.offsetY :xs + this.config.offsetX)
+            })
+            const bstr = n.join(",")
+            return (({
+                m:()=>`ctx.moveTo(${bstr})`,
+                c:()=>`ctx.bezierCurveTo(${bstr})`,
+                q:()=>`ctx.quadraticCurveTo(${bstr})`,
+                l:()=>`ctx.lineTo(${bstr})`,
+            }[e[0].match(/^[a-z]/img)[0].toLowerCase()]) || ((e)=>e))(e);
+        });
+        result = ['ctx.beginPath()'].concat(result).concat(['ctx.closePath()'])
+        if(this.config.fill){
+            result = result.concat(["ctx.fill()"])
+        }
+        return result.join(";\n")
+    }
+}
+
 export interface ObjectBaseType {
     isInside?(): boolean
 
     draw?(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement): Promise<any> | void
+
+    drawRotation?(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement): Promise<any> | void
 
     panstart?(event: any): [xName: string, yName: string]
 
@@ -85,50 +134,65 @@ const CanvasInteraction = defineComponent({
         const {width, height} = useWindowSize()
         const objectCache = ref([])
         const {x, y} = useMouseInElement(canvas)
-        const {Shift} = useMagicKeys({
+        const {Shift, Alt} = useMagicKeys({
             onEventFired(e) {
                 console.log(e.key)
             }
         })
         const isPan = ref(false)
+        const iconRotationRef = ref<HTMLImageElement>(null)
+
+        ;(async ()=>{
+            const iconRotation = `<svg t="1682039044848" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2655" data-spm-anchor-id="a313x.7781069.0.i1" width="200" height="200"><path d="M924 192c-19.8 0-36 16.2-36 36v40.3c-17-26.2-36.8-50.6-59.2-73.1-41.1-41.1-89-73.4-142.4-96C631.1 75.9 572.5 64 512 64S392.9 75.9 337.6 99.2c-53.4 22.6-101.3 54.9-142.4 96s-73.4 89-96 142.4C75.9 392.9 64 451.5 64 512s11.9 119.1 35.2 174.4c22.6 53.4 54.9 101.3 96 142.4 41.1 41.1 89 73.4 142.4 96C392.9 948.1 451.5 960 512 960c76.4 0 151.9-19.6 218.2-56.6 64.3-35.9 119.3-87.6 159.1-149.6 10.3-16.1 5.7-37.5-10.4-47.8-16.1-10.3-37.5-5.7-47.8 10.4-33.7 52.5-80.2 96.2-134.6 126.6-56 31.3-119.8 47.8-184.4 47.8-51.2 0-100.8-10-147.4-29.8-45.1-19.1-85.6-46.4-120.4-81.2C209.4 745 182 704.5 163 659.4c-19.7-46.7-29.8-96.3-29.8-147.4 0-51.2 10-100.8 29.8-147.4 19.1-45.1 46.4-85.6 81.2-120.4C279 209.4 319.5 182 364.6 163c46.7-19.7 96.3-29.8 147.4-29.8 51.2 0 100.8 10 147.4 29.8 45.1 19.1 85.6 46.4 120.4 81.2C814.6 279 842 319.5 861 364.6c13.7 32.5 22.7 66.3 27 101.2V516c0 19.8 16.2 36 36 36s36-16.2 36-36V228c0-19.8-16.2-36-36-36z" fill="#ffffff" p-id="2656"></path><path d="M235.7 457.2c0-5.3 4.9-9.9 11.5-10.6 17.6-1.9 17.1-3 24.8-6.6 7.8-3.6 15.2-13 19.7-24.8 1.7-4.5 6.8-7.6 12.6-7.6h8.4c7.3 0 13.3 4.8 13.3 10.7v187.5c0 5.9-5.9 10.7-13.3 10.7h-14.4c-7.3 0-13.3-4.8-13.3-10.7V478.6c0-5.9-5.9-10.7-13.3-10.7H249c-7.3 0-13.3-4.8-13.3-10.7zM390.4 491.4c-7.7-7.8-11.6-18-11.6-30.5 0-15.6 5.7-29.1 17-40.3 11.3-11.2 27.4-16.8 48.1-16.8 20.1 0 35.8 5.3 47.3 15.9 11.4 10.6 17.1 23 17.1 37.1 0 13.1-3.3 23.7-10 31.8-3.7 4.6-9.5 9.1-17.3 13.5 8.7 4 15.6 8.6 20.6 13.8 9.3 9.8 14 22.5 14 38.1 0 18.5-6.2 34.1-18.6 46.9-12.4 12.8-29.9 19.3-52.6 19.3-20.4 0-37.7-5.5-51.8-16.6-14.1-11.1-21.2-27.2-21.2-48.3 0-12.4 3-23.1 9.1-32.2 6.1-9 15-15.9 27-20.7-7.3-3.1-13-6.8-17.1-11z m79.8 89.7c6.7-5.5 10.1-13.7 10.1-24.5 0-11.3-3.5-19.9-10.4-25.7-6.9-5.8-15.8-8.8-26.6-8.8-10.5 0-19 3-25.6 9-6.6 6-9.9 14.2-9.9 24.8 0 9.1 3 17 9.1 23.6 6 6.6 15.4 9.9 28.1 9.9 10 0 18.5-2.8 25.2-8.3zM463.9 477c4.9-4.9 7.4-10.8 7.4-17.5 0-5.9-2.4-11.3-7.1-16.3-4.7-4.9-11.9-7.4-21.5-7.4s-16.5 2.5-20.7 7.4c-4.3 4.9-6.4 10.7-6.4 17.4 0 7.5 2.8 13.3 8.3 17.5 5.5 4.2 12 6.3 19.5 6.3 8.7 0 15.6-2.5 20.5-7.4zM668.7 437.8c9.4 17.3 14.1 41 14.1 71 0 28.5-4.2 52.1-12.7 70.8-12.3 26.8-32.4 40.1-60.4 40.1-25.2 0-43.9-10.9-56.2-32.8-10.3-18.3-15.4-42.8-15.4-73.5 0-23.8 3.1-44.3 9.2-61.4 11.5-31.8 32.4-47.8 62.5-47.8 27.2 0.1 46.8 11.2 58.9 33.6z m-32.4 127.6c6.6-9.9 9.9-28.3 9.9-55.2 0-19.4-2.4-35.4-7.2-47.9-4.8-12.5-14.1-18.8-27.9-18.8-12.7 0-22 6-27.8 17.9-5.9 11.9-8.8 29.4-8.8 52.6 0 17.4 1.9 31.4 5.6 42 5.7 16.2 15.6 24.2 29.4 24.2 11.3 0 20.2-4.9 26.8-14.8zM787.5 417.2c8.5 8.5 12.7 19 12.7 31.3 0 12.7-4.4 23.3-13.3 31.7-8.8 8.4-19.1 12.7-30.8 12.7-13 0-23.6-4.4-31.9-13.2-8.3-8.8-12.5-19-12.5-30.8 0-12.5 4.4-23 13.1-31.6 8.7-8.6 19.2-12.9 31.3-12.9 12.5 0 22.9 4.3 31.4 12.8z m-45.3 45.2c3.9 3.9 8.5 5.8 13.9 5.8 5.2 0 9.8-1.9 13.6-5.7 3.9-3.8 5.8-8.5 5.8-13.9 0-5.3-1.9-9.8-5.7-13.7-3.8-3.9-8.4-5.8-13.7-5.8-5.3 0-10 1.9-13.8 5.7-3.9 3.8-5.8 8.5-5.8 13.9 0 5.3 1.9 9.9 5.7 13.7z" fill="#ffffff" p-id="2657"></path></svg>`
+            const iconRotationSrc = `data:image/svg+xml;base64,${btoa(iconRotation)}`
+            const img:HTMLImageElement = await new Promise(resolve => {
+                const img = new Image()
+                img.src = iconRotationSrc
+                img.onload = ()=>{
+                    resolve(img)
+                }
+            })
+            iconRotationRef.value = img
+        })()
+
         watchEffect(() => {
             if (canvas) {
                 canvas.width = props.width || width.value
                 canvas.height = props.height || height.value
             }
         })
-        const positionMap = ref({
+        const positionMap = computed(()=>({
             top_left: {
-                cursor: 'nw-resize'
+                cursor: 'nwse-resize'
             },
             top_center: {
-                cursor: 'n-resize'
+                cursor: 'ns-resize'
             },
             top_right: {
-                cursor: 'ne-resize'
+                cursor: 'nesw-resize'
             },
             bottom_left: {
-                cursor: 'sw-resize'
+                cursor: 'nesw-resize'
             },
             bottom_center: {
-                cursor: 's-resize'
+                cursor: 'ns-resize'
             },
             bottom_right: {
-                cursor: 'se-resize'
+                cursor: 'nwse-resize'
             },
             left_center: {
-                cursor: 'w-resize'
+                cursor: 'ew-resize'
             },
             right_center: {
-                cursor: 'e-resize'
+                cursor: 'ew-resize'
             },
             content: {
-                cursor: 'move'
+                cursor:Alt.value ? 'ns-resize' : 'move'
             },
             blank: {
                 cursor: 'pointer'
             },
-        })
+        }))
 
         class ObjectBase implements ObjectBaseType {
             constructor(public x: number = 0, public y: number = 0, public w?: number, public h?: number) {
@@ -209,6 +273,28 @@ const CanvasInteraction = defineComponent({
                     return 'right_center'
                 }
                 return sx > 0 && sx < this.w && sy > 0 && sy < this.h ? 'content' : 'blank'
+            }
+
+            async drawRotation(ctx:CanvasRenderingContext2D, canvas:HTMLCanvasElement){
+                if(!iconRotationRef.value || !Alt.value) {
+                    return
+                }
+                let size = this.width * 0.3
+                if(this.height < this.width){
+                    size = this.height * 0.3
+                }
+                ctx.drawImage(
+                    iconRotationRef.value,
+                    0,
+                    0,
+                    iconRotationRef.value.width,
+                    iconRotationRef.value.height,
+                    this.centerX - size /2,
+                    this.centerY - size /2,
+                    size,
+                    size
+                )
+
             }
         }
 
@@ -519,6 +605,7 @@ const CanvasInteraction = defineComponent({
                         await object.draw(ctx, canvas)
                         if (props.showHelp && currObject.value === object) {
                             await drawAuxiliaryLine(object)
+                            await object.drawRotation?.(ctx, canvas)
                         }
                     }
                     k += 1
@@ -526,7 +613,6 @@ const CanvasInteraction = defineComponent({
                 requestAnimationFrame(_render)
             })()
         }
-        const a = $ref()
         onMounted(async () => {
             await render()
         })
