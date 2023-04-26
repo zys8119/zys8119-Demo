d7b5bd2f08afc3a432b80b09bd81f93faeaec616
zys8119 <770959294@qq.com>
2023-04-26 13:56:05 +0800

    [add] canvas 交互

--- src/views/PathRecognition.vue
+++ src/views/PathRecognition.vue
@@ -27,7 +27,7 @@ const load = async ({ObjectsClass, scene, ObjectBase}) => {
     scene.push(new ObjectsClass.Rect("#f500d5", 90, 200, 200, 500))
     scene.push(new ObjectsClass.Rect("#f500d5", 500, 100, 200, 500))
     const sceneObjs = scene.filter(e => e.type !== 'line')
-
+    const isInScene = (x, y)=> sceneObjs.find(e=> containsPoint(e, x, y))
     class Line extends ObjectBase {
         type = 'line'
         x = 0
@@ -58,28 +58,26 @@ const load = async ({ObjectsClass, scene, ObjectBase}) => {
             const b = Math.abs(y-this.ey);
             return Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
         }
-        async getPoint() {
-            const offset = 1
+        async getPoint(sx, sy, index = 0) {
+            const offset = 2
             const [x, y] = [
-                [this.sx, this.sy - offset],
-                [this.sx + offset, this.sy],
-                [this.sx, this.sy+offset],
-                [this.sx- offset, this.sy],
+                [sx, sy - offset],
+                [sx + offset, sy],
+                [sx, sy+offset],
+                [sx- offset, sy],
             ].reduce((a:any, b:any)=>{
                 const index = this.getDistance(b)
-                if(index < a[2]){
+                if(!isInScene(b[0], b[1]) && index < a[2]){
                     return [b[0], b[1], index]
                 }else {
                     return a
                 }
             }, [0, 0, Infinity])
-            this.sx = x
-            this.sy = y
-            this.ctx.moveTo(x, y)
-            if(this.getDistance([x, y]) <= 0){
+            if(index > 100 && x === this.ex && y === this.ey){
                 return
             }
-            await this.getPoint()
+            this.ctx.lineTo(x, y)
+            await this.getPoint(x,y, index+1)
         }
 
         async draw(ctx: CanvasRenderingContext2D, canvas) {
@@ -93,7 +91,7 @@ const load = async ({ObjectsClass, scene, ObjectBase}) => {
             this.ex = this.mousePoint[0]
             this.ey = this.mousePoint[1]
             ctx.moveTo(this.sx, this.sy)
-            await this.getPoint()
+            await this.getPoint(this.sx, this.sy)
             ctx.lineTo(this.mousePoint[0], this.mousePoint[1])
             ctx.stroke()
         }
