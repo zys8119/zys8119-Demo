90839e41d55c9ccae445a855585314c9d852d0b7
zys8119 <770959294@qq.com>
2023-06-07 17:19:16 +0800

    [add] windicss

---
+++ src/views/aa.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="aa">
+        撒打算大
+    </div>
+</template>
+
+<script setup lang="ts">
+import {createTemplatePromise} from "@vueuse/core"
+</script>
+
+<style scoped lang="less">
+.aa {
+
+}
+</style>
