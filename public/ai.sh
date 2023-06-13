c603db574e46ae29395eef45743e7c85df07767b
zys8119 <770959294@qq.com>
2023-06-13 17:32:14 +0800

    [add] order.vue

--- public/ai.sh
+++ public/ai.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 python3 -m pandora --tokens_file /Users/zhangyunshan/work/zys8119-Demo/public/chat-openai-token.json $*
+
+# 1. sudo cp /Users/zhangyunshan/work/zys8119-Demo/public/ai.sh /usr/local/bin/ai
+# 2. sudo chmod 777 /usr/local/bin/ai
