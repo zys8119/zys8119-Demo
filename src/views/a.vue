18c09155fb478eb659fff6de839ae8a5af853189
zys8119 <770959294@qq.com>
2023-08-04 15:16:01 +0800

    asdasdas

---
+++ src/views/a.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="a">
+        <div v-for="item, i in aa" :key="i">{{ i }}</div>
+    </div>
+</template>
+
+<script setup lang="ts">
+// 数组去重复
+const aa = ref<{
+    a?:string
+    b?:1222
+    [k:string]:any
+}[]>([
+    {
+        name:'asdas'
+    }
+])
+</script>
+
+<style scoped lang="less">
+.a {
+
+}
+</style>
