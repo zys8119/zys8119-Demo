399b23abfcc4c90c52771b82c208262ef3190584
zys8119 <770959294@qq.com>
2023-09-20 17:33:31 +0800

    [add] 考题

--- src/views/a.vue
+++ src/views/a.vue
@@ -1,5 +1,16 @@
 <template>
     <div class="a">
+        <div class="grid">
+            <div class="cell"></div>
+            <div class="cell"></div>
+            <div class="cell"></div>
+            <div class="cell"></div>
+            <div class="cell"></div>
+            <div class="cell"></div>
+            <div class="cell"></div>
+            <div class="cell"></div>
+            <div class="cell"></div>
+        </div>
     </div>
 </template>
 
@@ -11,7 +22,26 @@
 
 <style scoped lang="less">
 .a {
-    .aa{
+    .grid {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-template-rows: repeat(3, 1fr);
+        width: 300px; /* 适应你的布局尺寸 */
+        grid-gap: 5px;
     }
+
+    .cell {
+        background-color: #ccc;
+        transition: flex 0.3s;
+        width: 100%; /* 初始占比1:1 */
+        height: 100%; /* 初始占比1:1 */
+    }
+
+    .cell:hover {
+        grid-column: span 2; /* 占比2:1 */
+        grid-row: span 2; /* 占比2:1 */
+    }
+
+
 }
 </style>
