59872cf77cf56fa2366b88ece804da31634cfe72
zys8119 <770959294@qq.com>
2023-09-21 15:17:49 +0800

    [add] 考题

--- src/views/examination-paper/6.vue
+++ src/views/examination-paper/6.vue
@@ -5,7 +5,24 @@
 </template>
 
 <script setup lang="ts" title="6. 利用位运算实现权限组合">
-console.log(111)
+const read =  1
+const write = 2
+const admin = 4
+const guest = 8
+const user = 16
+let permission = read | write | admin | guest
+// 组合后的二进制
+console.log(permission.toString(2))
+// 添加
+permission |= user
+// 判断权限
+console.log(permission & user)
+// 删除权限
+permission ^= write
+console.log(permission & write)
+// 仅保留此权限
+permission &= guest
+console.log(permission & read)
 </script>
 
 <style scoped lang="less">
