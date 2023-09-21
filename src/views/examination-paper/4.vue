bee1ef5473156c3e4486f0e170d82aaa5ae6cacc
zys8119 <770959294@qq.com>
2023-09-21 14:50:08 +0800

    [add] 考题

--- src/views/examination-paper/4.vue
+++ src/views/examination-paper/4.vue
@@ -3,14 +3,15 @@
         vue实现刷新排序动画
         <div class="m-t-15">
             <button class="w-20 m-b-15px" @click="refresh">点击刷新</button>
-            <ul class="b-#f00 b-solid" ref="ulRef">
+            <ul class="b-#f00 b-solid" ref="ulRef" v-if="show">
                 <li :ref="el=> item.el = el" class="lh-50px m-b-5px" :style="{background:item.color}" v-for="(item,i) in list" :key="i">{{item.name}}</li>
             </ul>
+            <pre>{{JSON.stringify(list, null, 4)}}</pre>
         </div>
     </div>
 </template>
 
-<script setup lang="ts">
+<script setup lang="ts" title="排序动画">
 const list = ref<Array<{
     name:string
     color?:string
@@ -23,42 +24,66 @@ const list = ref<Array<{
     {name:"E", color:"#e5e5e5", el:null},
 ])
 const ulRef = ref<HTMLDivElement>()
-const animation = (start:number, end:number)=>{
-    const el = list.value[start].el
-    const endEl = list.value[end]?.el
-    const top = el.getBoundingClientRect().top
-    // ulRef.value.insertBefore(el, endEl)
-    // const top2 = el.getBoundingClientRect().top
-    // el.style.transform = `translateY(${top - top2}px)`
-    // requestAnimationFrame(()=>{
-    //     el.style.transition = `transform 1s`
-    //     el.style.transform = `translateY(0px)`
-    // })
+const time = ref(0.5)
+const show = ref(true)
+const animation = (el:HTMLDivElement, index:number)=>{
+    el.style.transform = `translateY(${index}px)`
+    requestAnimationFrame(()=>{
+        el.style.transition = `transform ${time.value}s`
+        el.style.transform = 'translateY(0px)'
+        el.style.removeProperty('transform')
+    })
+}
+const updateSort = (map)=>{
+    const newData = []
+    list.value.forEach((e,k)=>{
+        const Obj = map.find(e=>e.start === k)
+        if(Obj){
+            newData[Obj.end] = list.value[k]
+        }else {
+            newData[k] = e
+        }
+    })
+    list.value = newData
+    show.value = false
+    nextTick(()=>{
+        show.value = true
+    })
 }
 const reload = (start:number, end:number)=>{
+    const isOrder = start <= end
     const targets = list.value.reduce((a,b,k) => {
-        const isOrder = start <= end
         if(isOrder && k >= start && k <= end || !isOrder && k >= end && k <= start){
             a.push({
                 name:b.name,
+                el:b.el,
                 start:k,
                 end:k === start ? end : isOrder ? k-1 : k+1
             })
         }
         return a
     },[])
-    console.table(targets)
-    targets.forEach(({start, end})=>{
-        animation(start, end)
+    const startObj = isOrder ? targets[0] : targets[targets.length - 1]
+    const el = list.value[startObj.start].el
+    const endEl = list.value[startObj.end+(isOrder ? 1 : 0)]?.el
+    const startTops = targets.map(e => {
+        return e.el.getBoundingClientRect().top
     })
+    ulRef.value.insertBefore(el, endEl)
+    const endTops = targets.map(e => {
+        return e.el.getBoundingClientRect().top
+    })
+    startTops.forEach((e, k)=>{
+        const obj = targets[k]
+        animation(obj.el, e - endTops[k])
+    })
+    setTimeout(()=>{
+        updateSort(targets)
+    }, time.value*1000)
 }
 const refresh = ()=>{
-    reload(0,3)
+    reload(Math.floor(Math.random()*list.value.length),Math.floor(Math.random()*list.value.length))
 }
-onMounted(()=>{
-    reload(0,4)
-    reload(4,0)
-})
 
 </script>
 
