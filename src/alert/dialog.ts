dcad849fa23b6fe825ae4d8b1a2632c31bf99687
zys8119 <770959294@qq.com>
2023-10-24 11:13:25 +0800

    [add] 考题

--- src/alert/dialog.ts
+++ src/alert/dialog.ts
@@ -8,14 +8,19 @@ type DialogConfigType = {
     props?: Record<string, any>;
     width?: string | undefined;
 };
-const dialogCaches:Array<DialogReactive> = []
-const dialogDefault = (config: DialogConfigType = {} as DialogConfigType) => {
+const dialogCaches: Array<DialogReactive> = [];
+interface DialogDefault {
+    (config: DialogConfigType):DialogReactive
+    close():void
+    closeAll():void
+}
+const dialogDefault:DialogDefault = (config: DialogConfigType = {} as DialogConfigType) => {
     const dialogApp = dialog.create({
         title: config.title
             ? () =>
-                h(dialogAlertTitle, {
-                    title: config.title,
-                })
+                  h(dialogAlertTitle, {
+                      title: config.title,
+                  })
             : undefined,
         class: 'alert-dialog-custom-theme',
         style: `width:${config.width || 'auto'}`,
@@ -23,27 +28,27 @@ const dialogDefault = (config: DialogConfigType = {} as DialogConfigType) => {
         content: () =>
             typeof config.content === 'object'
                 ? h(App, [
-                    h(
-                        defineAsyncComponent({
-                            loader: () => config.content,
-                        }),
-                        config.props
-                    ),
-                ])
+                      h(
+                          defineAsyncComponent({
+                              loader: () => config.content,
+                          }),
+                          config.props
+                      ),
+                  ])
                 : config.content,
     } as any);
-    dialogCaches.push(dialogApp)
-    return dialogApp
+    dialogCaches.push(dialogApp);
+    return dialogApp;
 };
-dialogDefault.close = ()=>{
-    const a = dialogCaches.pop()
-    setTimeout(()=>{
-        a?.destroy()
-    }, 500)
-}
-dialogDefault.closeAll = ()=>{
-    while (dialogCaches.length > 0){
-        dialogDefault.close()
+dialogDefault.close = () => {
+    const dialogPop = dialogCaches.pop();
+    setTimeout(() => {
+        dialogPop?.destroy();
+    }, 500);
+};
+dialogDefault.closeAll = () => {
+    while (dialogCaches.length > 0) {
+        dialogDefault.close();
     }
-}
-export default dialogDefault
+};
+export default dialogDefault;
