50335190c8a1eb7d00ae409b0c11e69af0eccb2c
zys8119 <770959294@qq.com>
2023-10-26 09:43:51 +0800

    [add] 考题

--- src/views/examination-paper/5.vue
+++ src/views/examination-paper/5.vue
@@ -6,21 +6,25 @@
 
 <script setup lang="ts" title="怎样使 const [a, b] = {a:1, b:2}">
 /**
+ * 方案1
  * 怎样使 const [a, b] = {a:1, b:2}
  */
 Object.prototype[Symbol.iterator] = function (){
     return Object.values(this)[Symbol.iterator]()
 }
-Object.prototype[Symbol.iterator] = function *(){
-    for (let v of Object.values(this)){
-        yield v
-    }
-}
+// 方案二
+// Object.prototype[Symbol.iterator] = function *(){
+//     for (let v of Object.values(this)){
+//         yield v
+//     }
+// }
 const [a, b] = {
     a:1,
     b:2,
 } as any
 console.log(a, b)
+
+
 </script>
 
 <style scoped lang="less">
