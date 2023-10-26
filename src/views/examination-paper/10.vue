c8849069aa6f9ca79344289dbee3e718fe1f841f
zys8119 <770959294@qq.com>
2023-10-26 16:46:39 +0800

    [add] 考题

--- src/views/examination-paper/10.vue
+++ src/views/examination-paper/10.vue
@@ -5,63 +5,39 @@
 </template>
 
 <script setup lang="ts">
-// function maxPoints(points: number[][]): number {
-//     const calc = (curr, row, col)=>{
-//         const rowArr = points[row -1] ? points[row -1] : [...points[row]].fill(0)
-//         const map = rowArr.map((e, k)=> {
-//             return e + curr - Math.abs(k - col)
-//         })
-//         const max = Math.max.apply(null, map)
-//         return max
-//     }
-//     points.forEach((e, row)=>{
-//         e.forEach((v, col)=>{
-//             points[row][col] = calc(v, row, col)
-//         })
-//     })
-//     return Math.max.apply(null, points.at(-1))
-// };
 import a from "./a"
-let points = [[1,2,4,4,3], [1,4,5,5,1], [5,2,3,5,5], [2,3,4,3,0]] // 17
+let points = [
+    [1,2,4,4,3],
+    [1,4,5,5,1],
+    [5,2,3,5,5],
+    [2,3,4,3,0]
+] // 17
 // points = [[1,2,3],[1,5,1],[3,1,1]] // 9
 // points = [[1,5],[2,3],[4,2]] // 11
 // points = a // ?
 
 console.table(points)
-console.time()
-let prevRowSum = []
-const run = (row)=>{
-    let max = 0
-    const maxMap = {}
-    points[row].forEach((e,k)=>{
-        points[row].forEach((_, kk)=>{
-            const pv = (prevRowSum[kk] || 0)
-            const sum = e + pv - Math.abs(k - kk)
-            maxMap[sum] = maxMap[sum] || []
-            maxMap[sum].push({
-                row:row,
-                col:k,
-                value:sum
-            })
-            if(sum > max){
-                max = sum
-            }
-        })
-    })
-    prevRowSum = []
-    const arr = maxMap[max] || []
-    arr.forEach(e=>{
-        points[e.row][e.col] = e.value
-        prevRowSum[e.col] = e.value
-    })
-}
-points.forEach((_, k)=>{
-    run(k)
+
+function maxPoints(points: number[][]): number {
+    const fn = (i, j, vv)=>(points[i -1] || []).reduce((a, b, j2)=>{
+        const sum = b - Math.abs(j - j2)
+        return sum > a ? sum : a
+    }, 0) + vv
+    const m = points.length - 1
+    const n = points[0].length - 1
+    for (let i = 0; i < m; i++){
+        for (let j = 0; j < n; j++){
+            points[i][j] = fn(i, j, points[i][j])
+        }
+    }
+    return points.at(-1).reduce((a,b)=>a > b ? a : b,0)
+};
+onMounted(()=>{
+    // fn[i][j] = Max{f[i-1][j2] - j + j2} + points[i][j]
+
+    console.table(maxPoints([[1,2,3],[1,5,1],[3,1,1]]));
 })
 
-console.log(Math.max.apply(null, points.at(-1)))
-console.timeEnd()
-console.table(points)
 </script>
 
 <style scoped lang="less">
