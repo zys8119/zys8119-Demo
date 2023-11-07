5b5bb400628ce35cea73ac2f50e50403bbc10e6f
zys8119 <770959294@qq.com>
2023-11-07 21:44:16 +0800

    [add] pdfview.vue

---
+++ docs/assets/橙色大方床-24428fe9.js
@@ -0,0 +1,1 @@
+const a=""+new URL("橙色大方床-1acd9ca4.obj",import.meta.url).href;export{a as default};
---
+++ src/views/pdfview.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="b flex flex-col items-center p-15px gap-15px">
+    <canvas class="b b-solid" ref="canvasRef"></canvas>
+  </div>
+</template>
+
+<script setup lang="ts">
+import pdfjsLibSrc from "pdfjs-dist/build/pdf.mjs?url"
+import pdfWorker from "pdfjs-dist/build/pdf.worker.mjs?url"
+const canvasRef = ref()
+const router = useRoute()
+
+const {load} = useScriptTag(pdfjsLibSrc, undefined, {
+  type:"module"
+})
+onMounted(async ()=>{
+  await load()
+  pdfjsLib.GlobalWorkerOptions.workerSrc = pdfWorker
+  const loadingTask = pdfjsLib.getDocument(router.query.file)
+  const pdf = await loadingTask.promise
+  console.log('PDF loaded');
+  const page = await pdf.getPage(1)
+  console.log('Page loaded');
+  const scale = 1.5;
+  const viewport = page.getViewport({scale: scale});
+  const canvas = canvasRef.value;
+  const context = canvas.getContext('2d');
+  canvas.height = viewport.height;
+  canvas.width = viewport.width;
+  // Render PDF page into canvas context
+  var renderTask = page.render( {
+    canvasContext: context,
+    viewport: viewport
+  });
+  console.log(renderTask)
+  await renderTask.promise
+  console.log('Page rendered');
+})
+</script>
+
+<style scoped lang="less">
+
+</style>
