68432bdada5cd88c5f5cad9d5919c725ba31daaa
zys8119 <770959294@qq.com>
2023-11-27 13:33:10 +0800

    [add] draggable

---
+++ src/views/bayl.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bayl">
+    <img class="absolute w-100% h-100% block" :src="`./images/bayl.jpg`" alt="">
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="less">
+.bayl {
+
+}
+</style>
