25512d354ac845d121f979cc444dfd90022b3f9a
zys8119 <770959294@qq.com>
2023-12-07 18:02:16 +0800

    [add] yml

--- src/views/bbb.vue
+++ src/views/bbb.vue
@@ -52,6 +52,7 @@ onMounted(async ()=>{
         break
       case 'bb':
           if(peerConnectionMap.has(data.userId)){
+            console.log(data.data)
             await peerConnectionMap.get(data.userId).setRemoteDescription(data.data)
           }
         break
