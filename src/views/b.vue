2e1031c1823f96c95742787370aaeea09b46e239
zys8119 <770959294@qq.com>
2024-02-23 08:58:16 +0800

    [add] 新改版v3

---
+++ src/views/b.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="b">
+
+  </div>
+</template>
+
+<script setup lang="ts">
+console.log(await fetch('https://www.baidu.com/').then(res=>res.text()))
+</script>
+
+<style scoped lang="less">
+.b {
+
+}
+</style>
+This is the rendered form of the equation. You can not edit this directly. Right click will give you the option to save the image, and in most browsers you can drag the image onto your desktop or another program.
