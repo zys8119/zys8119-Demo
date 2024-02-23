c6032a1fa578beb2ff21526840a7c354f304a89a
zys8119 <770959294@qq.com>
2024-02-23 09:00:38 +0800

    [add] 新改版v3

--- src/views/b.vue
+++ src/views/b.vue
@@ -13,4 +13,3 @@ console.log(await fetch('https://www.baidu.com/').then(res=>res.text()))
 
 }
 </style>
-This is the rendered form of the equation. You can not edit this directly. Right click will give you the option to save the image, and in most browsers you can drag the image onto your desktop or another program.
