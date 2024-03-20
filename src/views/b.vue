178bf68e6d40baa5ec7f117e72ad9e8e6d2fe208
zys8119 <770959294@qq.com>
2024-03-20 09:34:06 +0800

    [add] 新改版v3

--- src/views/b.vue
+++ src/views/b.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="b">
-
+  <div class="b" ref="aa">
+      <div class="aa w-50px h-50px bg-#f00"></div>
+    https://developers.weixin.qq.com/community/minihome/doc/0008c82534c690acd211d0faa6bc00
   </div>
 </template>
 
 <script setup lang="ts">
-console.log(await fetch('https://www.baidu.com/').then(res=>res.text()))
+
 </script>
 
 <style scoped lang="less">
 .b {
-
 }
 </style>
