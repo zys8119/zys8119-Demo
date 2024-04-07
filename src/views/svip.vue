7d4397f4dfc121597982f0e7a69b9366e7864e10
zys8119 <770959294@qq.com>
2024-04-07 14:52:01 +0800

    [add] test

--- src/views/svip.vue
+++ src/views/svip.vue
@@ -7,14 +7,27 @@
     '--n-color-focus':'#fff2',
     }" class="bg-#fff2 b-rd-25px text-#fff !w-50% m-auto abs-x top-50px"
              clearable
-             placeholder="请输入关键词" v-model:value="search" @keyup.enter="onSearch"></n-input>
+             placeholder="请输入关键词" v-model:value="search" @keyup.enter="onSearch">
+      <template #suffix>
+        <n-button type="primary" @click="onSearch">搜索</n-button>
+      </template>
+    </n-input>
   </div>
 </template>
 
 <script setup lang="ts">
+import axios from "axios"
 const search = ref()
-const onSearch = () => {
+const onSearch = async () => {
   console.log(search.value)
+  const res = await axios({
+    url:"http://127.0.0.1:82/",
+    method:"get",
+    params:{
+      search:search.value
+    }
+  })
+  console.log(res.data)
 }
 </script>
 
