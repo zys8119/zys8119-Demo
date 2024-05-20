75adcd87cd23b950aba22eed44a8e99f9c39d9b6
zys8119 <770959294@qq.com>
2024-05-20 18:04:31 +0800

    [add] 任务并发控制

--- a.ts
+++ a.ts
@@ -2,3 +2,16 @@ declare module "mockjs-async"{
     import * as Mockjs from "mockjs"
     export = Mockjs
 }
+
+declare module "protable" {
+    interface FromArray{
+        (selector:string,data:Array<Record<any, any>>, options:{
+            contents:Record<any, (content:any)=> any>
+        }):void
+    }
+    export const fromArray:FromArray
+}
+
+declare module "*.vue"{
+    export  {}
+}
