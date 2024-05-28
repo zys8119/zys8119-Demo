dbe953237be0cacb666d87ba350841ace4b86421
zys8119 <770959294@qq.com>
2024-05-28 11:56:11 +0800

    [add] vue 加密

---
+++ git/release.sh
@@ -0,0 +1,31 @@
+cd dist
+git init
+git remote add origin 仓库地址
+git fetch
+result=$(git --no-pager log origin/main -1  | grep "commit .*" | sed "s/commit //g")
+result=$(git ls-tree $result | grep ".*package.json" | sed "s/.*blob \([a-z0-9]*\).*/\1/")
+result=$(git show $result | grep 'version' | sed "s/.*\"\([0-9.]*\)\".*/\1/")
+# 提取 MAJOR、MINOR 和 PATCH 版本号
+IFS='.' read -r major minor patch <<< "$result"
+# 递增 PATCH 版本号
+patch=$((patch + 1))
+# 检查 PATCH 是否达到 100
+if [ "$patch" -ge 100 ]; then
+    patch=0
+    minor=$((minor + 1))
+fi
+
+# 检查 MINOR 是否达到 100
+if [ "$minor" -ge 100 ]; then
+    minor=0
+    major=$((major + 1))
+fi
+# 生成新的版本号
+new_version="$major.$minor.$patch"
+
+echo "$(cat package.json | sed "s/\(version\": \"\).*\(\"\)/\1$new_version\2/")" > package.json
+
+git pull origin main
+git add .
+git commit -m "[add] update version $new_version"
+git push origin main
