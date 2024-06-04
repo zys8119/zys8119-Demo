ea885803db744a3e547dc9f6e55a4472f6032e60
zys8119 <770959294@qq.com>
2024-06-04 13:09:38 +0800

    [add] 飞盘布局

--- src/views/wujie-vue/resolver.d.ts
+++
@@ -1,7 +0,0 @@
-export interface Resolver {
-    ():ResolverResults
-}
-export type ResolverResults = Array<(name:string)=> ({name:string, from:string})>
-export declare const resolver:Resolver
-export default resolver
-export {}
