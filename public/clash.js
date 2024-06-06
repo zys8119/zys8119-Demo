a53864abc7f984549d5b906744930b752791b361
zys8119 <770959294@qq.com>
2024-06-06 13:51:50 +0800

    [add] 飞盘布局

--- public/clash.js
+++ public/clash.js
@@ -32,6 +32,6 @@ module.exports.parse = async (raw, { axios, yaml, notify, console }, { name, url
     ].map(i => `DOMAIN-SUFFIX,${i},${obj['proxy-groups'][0].name}`)
     return yaml.stringify({
         ...obj,
-        rules
+        rules: [...obj.rules, ...rules],
     })
 }
