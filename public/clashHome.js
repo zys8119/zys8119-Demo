ef1d932cfe104c052877c3c1d8ac4358fe4dbfa6
zys8119 <770959294@qq.com>
2024-06-15 16:02:15 +0800

    [add] build

---
+++ public/clashHome.js
@@ -0,0 +1,9 @@
+module.exports.parse = async (raw, { axios, yaml, notify, console }, { name, url, interval, selected }) => {
+    const obj = yaml.parse(raw)
+    const rules = [
+    ].map(i => `DOMAIN-KEYWORD,${i},${obj['proxy-groups'][0].name}`)
+    return yaml.stringify({
+        ...obj,
+        rules: rules.concat(obj.rules),
+    })
+}
