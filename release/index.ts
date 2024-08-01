7011711f58391b5087a2ef2f2fabcebc4f0c47aa
zys8119 <770959294@qq.com>
2024-08-01 17:05:21 +0800

    [add] update

--- release/index.ts
+++ release/index.ts
@@ -6,6 +6,11 @@ import createReleases from './createReleases';
 import argv from './argv';
 (async () => {
     try {
+        if (argv.syncTag) {
+            execSync(`git tag -l | xargs git tag -d`);
+            execSync(`git fetch --tags`);
+            return;
+        }
         const releases = createReleases();
         const rlist = await releases.list();
         const ref = execSync(`git log -n 1 --pretty=format:"%H"`).toString().trim();
@@ -18,17 +23,32 @@ import argv from './argv';
                 : rlist?.[0]?.name
                 ? execSync(`semver ${rlist?.[0]?.name} -i`).toString().trim()
                 : defaultVerions;
-        releases[argv.update ? 'update' : 'create']({
-            description: template(readFileSync(resolve(__dirname, '../changelog.md'), 'utf-8'))({
-                version,
-                commitHex: ref
-            }),
-            tag_name: version,
-            ref,
-            zipDir: 'dist/**',
-            filename: '前端部署资源包'
-        })
+        if (argv.update && typeof rlist?.[0]?.tag_name === 'string') {
+            const tag_name = rlist?.[0]?.tag_name;
+            execSync(`git tag -d ${tag_name}`);
+            execSync(`git push origin --delete ${tag_name}`);
+            execSync(`git tag -l | xargs git tag -d`);
+            execSync(`git fetch --tags`);
+            await releases.delete({
+                tag_name
+            });
+        }
+        releases
+            .create({
+                description: template(readFileSync(resolve(__dirname, '../changelog.md'), 'utf-8'))(
+                    {
+                        version,
+                        commitHex: ref
+                    }
+                ),
+                tag_name: version,
+                ref,
+                zipDir: 'dist/**',
+                filename: '前端部署资源包'
+            })
             .then((res) => {
+                execSync(`git tag -l | xargs git tag -d`);
+                execSync(`git fetch --tags`);
                 console.log(res);
             })
             .catch((err) => {
