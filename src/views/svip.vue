90c83a9269dda0d4c523331ef2b560c26a59712c
zys8119 <770959294@qq.com>
2024-12-11 13:37:29 +0800

    [add] update

--- src/views/svip.vue
+++ src/views/svip.vue
@@ -1,38 +1,96 @@
 <template>
   <div class="svip bg-#121833 abs-f w-100% h-100%">
     <n-input :style="{
-    '--n-border':'none',
-    '--n-height':'50px',
-    '--n-text-color':'#fff',
-    '--n-color-focus':'#fff2',
-    }" class="bg-#fff2 b-rd-25px text-#fff !w-50% m-auto abs-x top-50px"
-             clearable
-             placeholder="请输入关键词" v-model:value="search" @keyup.enter="onSearch">
+      '--n-border': 'none',
+      '--n-height': '50px',
+      '--n-text-color': '#fff',
+      '--n-color-focus': '#fff2',
+    }" class="bg-#fff2 b-rd-25px text-#fff !w-50% m-auto abs-x top-50px" clearable placeholder="请输入关键词"
+      v-model:value="search" @keyup.enter="onSearch">
       <template #suffix>
         <n-button type="primary" @click="onSearch">搜索</n-button>
       </template>
     </n-input>
+    <div class="abs-x top-100px w-100%  flex top-150px" :style="{ height: 'calc(100% - 200px)' }">
+      <div class="w-300px of-x-hidden">
+        <n-collapse class="w-100%">
+          <n-collapse-item v-for="(items, index) in lives" :key="index">
+            <template #header>
+              <div class="text-#fff">{{ index }}</div>
+            </template>
+            <div class="text-#fff" v-for="(items2, index) in items" :key="index">
+              <div class="text-#fff cursor-pointer lh-40px p-x-30px hover:text-#003cff hover:bg-#14162c hover:bg-op-50"
+                v-for="(item, index) in items2" :key="index" @click="onPlay(item)">
+                <n-ellipsis>{{ item.name }}</n-ellipsis>
+              </div>
+            </div>
+          </n-collapse-item>
+        </n-collapse>
+      </div>
+      <div class="flex-1">
+        <video id="video" class="w-100% h-100%" controls></video>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import axios from "axios"
+import Hls from "hls.js"
 const search = ref()
+const lives = ref({})
 const onSearch = async () => {
   console.log(search.value)
   const res = await axios({
-    url:"http://127.0.0.1:82/",
-    method:"get",
-    params:{
-      search:search.value
+    url: "http://127.0.0.1:82/",
+    method: "get",
+    params: {
+      search: search.value
     }
   })
   console.log(res.data)
 }
+onMounted(async () => {
+  const tvRes = await axios({
+    url: "http://127.0.0.1:82/svip/lives/proxy",
+    method: "get",
+  })
+  const tv = tvRes.data.data.split('\n')
+  const results = {}
+  let oneName = null
+  do {
+    const line = tv.shift().trim()
+    if (/#genre#/.test(line)) {
+      oneName = line.split(',')[0]
+      results[oneName] == results[oneName] || {}
+    } else if (line) {
+      const [name, url] = line.split(',')
+      if (!results[oneName]) {
+        oneName = name
+        results[oneName] = {}
+      }
+      results[oneName][name] = results[oneName][name] || []
+      results[oneName][name].push({
+        name,
+        url,
+        id: line
+      })
+    }
+  } while (tv.length > 0)
+  lives.value = results
+})
+const onPlay = async (item) => {
+
+  const video = document.getElementById('video') as HTMLVideoElement;
+  const hls = new Hls();
+  hls.loadSource(item.url);
+  hls.attachMedia(video);
+  hls.on(Hls.Events.MANIFEST_LOADED, () => {
+    video.play();
+  });
+}
 </script>
 
 <style scoped lang="less">
-.svip {
-
-}
+.svip {}
 </style>
