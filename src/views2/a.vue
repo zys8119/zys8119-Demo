471b369eb856764101015874024e5feb9d74008e
zys8119 <770959294@qq.com>
2025-06-30 09:00:37 +0800

    [add] tiktok

---
+++ src/views2/a.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class='a text-#fff s-200px bg-#f00 flex-center text-30px'>
+        sa
+    </div>
+</template>
+<script setup lang="ts" a="[1]" title="asdaSda" asdasdas=asda>
+console.log(222233)
+</script>
+<style scoped lang="less">
+.a {}
+</style>
