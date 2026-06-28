3d46e46e95fe0ce6b8e37b07a7ad45d42547de1a
zys8119 <770959294@qq.com>
2026-06-28 16:40:39 +0800

    feat(aa.vue): 更新页面UI并新增动态统计模块
    - 调整首图宽度从90%至80%
    - 重新设计顶部双卡片布局，添加循环渲染的统计列表
    - 为右侧卡片添加上轮答案展示与装饰图标
    - 缩小弹幕区域高度并添加占位容器
    - 新增chunk1响应式数据支持统计模块

--- src/views/aa.vue
+++ src/views/aa.vue
@@ -3,7 +3,7 @@
         <img class="abs-content" src="/images/cy/bg.png" alt="">
         <div class="abs-content flex-center justify-start flex-col">
             <div class="flex-center w-full mt-10px">
-                <img src="/images/cy/1.png" class="w-90%" alt="">
+                <img src="/images/cy/1.png" class="w-80%" alt="">
             </div>
             <div class="flex-center w-full mt-10px abs-r  w-60% h-30px">
                 <img class="abs-content" src="/images/cy/2.png" alt="">
@@ -14,11 +14,30 @@
             </div>
             <div class="flex-1  m-y-10px w-[calc(100%-20px)] flex flex-col gap-10px">
                 <div class="flex-center gap-10px justify-between w-full">
-                    <div class="flex-1 abs-r h-50px">
+                    <div class=" abs-r h-80px w-60%">
                         <img class="abs-content" src="/images/cy/5.png" alt="">
+                        <div class="abs-content flex flex-col gap-5px">
+                            <div class="flex-center mt-10px justify-between p-x-10px">
+                                <div class="flex-center gap-4px" v-for="(item, index) in chunk1" :key="index">
+                                    <img class="s-15px" :src="item.img" alt="">
+                                    <div class="text-12px">{{ item.text }}</div>
+                                </div>
+                            </div>
+                            <div class="flex-1">礼物</div>
+                        </div>
                     </div>
-                    <div class="flex-1 abs-r h-50px">
+                    <div class="flex-1 abs-r h-80px">
                         <img class="abs-content" src="/images/cy/6.png" alt="">
+                        <div class="abs-content">
+                            <div class="text-12px  mt-10px flex gap-5px p-x-10px">
+                                <div class="text-#999">上轮答案:</div>
+                                <div>asd</div>
+                            </div>
+                            <div class="p-x-10px">
+                                <div class="text-16px text-#c8f0f9">火腿肠</div>
+                            </div>
+                        </div>
+                        <img class="abs bottom-0 right-0 w-50px" src="/images/cy/8.png" alt="">
                     </div>
                 </div>
                 <div class="flex-1 flex-center gap-10px">
@@ -35,10 +54,13 @@
                         </div>
                     </div>
                 </div>
-                <div class="abs-r h-120px">
+                <div class="abs-r h-100px">
                     <img class="abs-content" src="/images/cy/7.png" alt="">
                     <div class="abs-center top-10px text-12px  text-#c8f0f9 text-shadow-xs text-shadow-#00ceee">
                         弹幕区域
+                    </div>
+                    <div class="abs-content  top-10px h-[calc(100%-10px)]">
+                        asda
                     </div>
                 </div>
             </div>
@@ -46,7 +68,20 @@
     </div>
 </template>
 <script setup lang="ts">
-
+const chunk1 = ref([
+    {
+        img: '/images/cy/11.png',
+        text: '字数'
+    },
+    {
+        img: '/images/cy/10.png',
+        text: '提示'
+    },
+    {
+        img: '/images/cy/9.png',
+        text: '倒计时'
+    }
+])
 </script>
 <style scoped lang="less">
 .aa {}
