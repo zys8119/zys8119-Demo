0a2ae34a7f2f06d84ab25c169851b79f5e3981a7
zys8119 <770959294@qq.com>
2026-06-28 16:19:12 +0800

    功能(aa页面): 更新页面布局并新增图片资源
    新增5张PNG图片至public/images/cy目录，重构aa.vue页面UI，替换原有红色占位色块与静态文本为主题图片和动态文字，添加弹幕区、猜词区与排行榜板块，优化整体布局与文字样式

--- src/views/aa.vue
+++ src/views/aa.vue
@@ -5,20 +5,42 @@
             <div class="flex-center w-full mt-10px">
                 <img src="/images/cy/1.png" class="w-90%" alt="">
             </div>
-            <div class="flex-center w-full mt-10px abs-r  w-60%">
-                <img src="/images/cy/2.png" alt="">
-                <div class="abs-content flex-center text-white">asdaq</div>
+            <div class="flex-center w-full mt-10px abs-r  w-60% h-30px">
+                <img class="abs-content" src="/images/cy/2.png" alt="">
+                <div
+                    class="abs-content flex-center bg-linear-to-r from-#57c0cd to-#fce173 bg-clip-text text-transparent">
+                    关注主播解锁彩色昵称!
+                </div>
             </div>
             <div class="flex-1  m-y-10px w-[calc(100%-20px)] flex flex-col gap-10px">
                 <div class="flex-center gap-10px justify-between w-full">
-                    <div class="bg-#f00 flex-1">1</div>
-                    <div class="bg-#f00 flex-1">1</div>
+                    <div class="flex-1 abs-r h-50px">
+                        <img class="abs-content" src="/images/cy/5.png" alt="">
+                    </div>
+                    <div class="flex-1 abs-r h-50px">
+                        <img class="abs-content" src="/images/cy/6.png" alt="">
+                    </div>
                 </div>
                 <div class="flex-1 flex-center gap-10px">
-                    <div class="bg-#f00 w-60% h-full">21</div>
-                    <div class="bg-#f00 flex-1 h-full">123</div>
+                    <div class=" w-60% h-full abs-r">
+                        <img class="abs-content" src="/images/cy/3.png" alt="">
+                        <div class="abs-center top-10px text-12px  text-#ffffff text-shadow-">
+                            猜词区域
+                        </div>
+                    </div>
+                    <div class=" flex-1 h-full abs-r">
+                        <img class="abs-content" src="/images/cy/4.png" alt="">
+                        <div class="abs-center top-10px text-12px  text-#ffffff text-shadow-">
+                            排行榜
+                        </div>
+                    </div>
                 </div>
-                <div class="bg-#f00">1</div>
+                <div class="abs-r h-120px">
+                    <img class="abs-content" src="/images/cy/7.png" alt="">
+                    <div class="abs-center top-10px text-12px  text-#c8f0f9 text-shadow-xs text-shadow-#00ceee">
+                        弹幕区域
+                    </div>
+                </div>
             </div>
         </div>
     </div>
